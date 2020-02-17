c71b183a9a4da9e3cf450b5dbd27fabc024978a2
UO258935 <UO258935@uniovi.es>
2020-02-17 09:35:24 +0100

    pmm

---
+++ lab03/src/parser/PmmVisitor.java
@@ -0,0 +1,19 @@
+// Generated from C:/Users/Marcos/Desktop/INFORMATICA/TERCERO/2/DLP/lab/DLP_1920/lab03/src/parser\Pmm.g4 by ANTLR 4.8
+package parser;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link PmmParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface PmmVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link PmmParser#program}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProgram(PmmParser.ProgramContext ctx);
+}
